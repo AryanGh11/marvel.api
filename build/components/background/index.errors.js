12efaa19f53831b30781e751c5f1286f702194f5
Aryan Gholami <mahdi.gholami875@gmail.com>
2023-09-24 23:30:26 +0330

    deleting build folder

--- build/components/background/index.errors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function BackgroundErrors(error) {
-    if (error.code === 11000) {
-        return "Document already existing";
-    }
-}
-exports.default = BackgroundErrors;
--- build/components/user/index.errors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function UserErrors(error) {
-    if (error.code === 11000) {
-        return "Document already existing";
-    }
-}
-exports.default = UserErrors;
